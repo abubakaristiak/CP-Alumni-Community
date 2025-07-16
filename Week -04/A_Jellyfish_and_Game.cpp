a5a27b7b960f49b60165a80fdf0af42cfc578fc2
Abu Bakar Istiak <abubakar119147@gmail.com>
2025-07-16 12:35:46 +0600

    added new problem

---
+++ Week -04/A_Jellyfish_and_Game.cpp
@@ -0,0 +1,90 @@
+/*
+||-----------------------------------||
+||        Abu Bakar Istiak           ||
+||     Chattogram Polytechnic        ||
+||        Department of CST          ||
+||    abubakar119147@gmail.com       ||
+||-----------------------------------||
+*/
+
+// https://codeforces.com/problemset/problem/1874/A
+
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+#define ll long long
+#define ull unsigned long long
+#define pi pair<ll, ll>
+#define vi vector<ll>
+#define vpi vector<pi>
+#define pb push_back
+#define endl "\n"
+#define sp " "
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define cyes cout << "Yes\n"
+#define cno cout << "No\n"
+#define sz(x) (ll)(x).size()
+#define minus cout << -1 << endl
+#define zero cout << 0 << endl
+#define MAX 100000
+#define MOD 998244353
+#define afor(i,a,b) for (ll i = (a); i < (b); ++i)
+#define rfor(i,a,b) for (ll i = (a); i >= (b); --i)
+#define asort(v) sort((v).begin(), (v).end())
+#define rsort(v) sort((v).begin(), (v).end(), greater<>())
+#define fast() ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+template <typename T>
+using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+
+void solve() {
+    int n,m,k; cin >> n >> m >> k;
+    vector<int> a(n),b(m);
+    for(int i=0; i<n; i++){
+        cin >> a[i];
+    }
+    for(int i=0; i<m; i++){
+        cin >> b[i];
+    }
+
+    asort(a);
+    asort(b);
+    if(k%2==0){
+        if(a.front()<b.back()){
+            swap(a.front(), b.back());
+            asort(a); asort(b);
+        }if(b.front()<a.back()){
+            swap(a.back(), b.front());
+            asort(a); asort(b);
+        }
+    }else{
+        if(a.front()<b.back()){
+            swap(a.front(), b.back());
+            asort(a); asort(b);
+        }
+    }
+    ll res=0;
+    for(int i=0; i<n; i++){
+        res+=a[i];
+    }
+    cout << res << endl;
+}
+
+
+int main() {
+    fast();
+    int t = 1;
+    cin >> t;
+    while(t--){
+        solve();
+    }
+    return 0;
+
+    // Alhamdulillah
+}
